73350ef62c720f2ced455bd9d04d2399c34fa1da
Mateja <mateja.putic@gmail.com>
2023-09-03 14:13:46 -0400

    Partiy with the notebook

--- gpt-nano.py
+++ gpt-nano.py
@@ -1,10 +1,29 @@
 from typing import List
 from datasets import load_dataset
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-d_train = load_dataset("tiny_shakespeare", split="train")[0]["text"]
-d_val = load_dataset("tiny_shakespeare", split="validation")[0]["text"]
+# Hyperparameters
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = "mps"
+eval_iters = 200
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
-vocab = sorted(list(set(d_train + d_val)))
+torch.manual_seed(1337)
+
+train_data = load_dataset("tiny_shakespeare", split="train")[0]["text"]
+val_data = load_dataset("tiny_shakespeare", split="validation")[0]["text"]
+
+vocab = sorted(list(set(train_data + val_data)))
+vocab_size = len(vocab)
 
 stoi = {ch: i for i, ch in enumerate(vocab)}
 itos = {i: ch for i, ch in enumerate(vocab)}
@@ -16,3 +35,65 @@ def encode(s: str):
 
 def decode(l: List):
     return "".join([itos[i] for i in l])
+
+
+train_data = torch.tensor(encode(train_data), dtype=torch.long)
+
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    return x, y
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        logits = self.token_embedding_table(idx)  # B T C
+
+        if targets is None:
+            loss = None
+        else:
+            # Reshape logits
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+xb, yb = get_batch("train")
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
+idx = torch.zeros((1, 1), dtype=torch.long)
+print(decode(m.generate(idx, max_new_tokens=100)[0].tolist()))
+
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+
+for steps in range(10000):
+    if steps % eval_interval == 0:
+        print()
+    xb, yb = get_batch("train")
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(loss.item())
