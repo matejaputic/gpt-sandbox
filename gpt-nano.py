27499bbf8ed0a151d7ddea96ca31e83be765b89f
Mateja <mateja.putic@gmail.com>
2023-09-03 12:04:47 -0400

    Added stoi and itos encode and decode

--- gpt-nano.py
+++ gpt-nano.py
@@ -1,4 +1,18 @@
-from typing import Dict
+from typing import List
+from datasets import load_dataset
 
-stoi: Dict = lambda chars: { ch: i for i, ch in enumerate(chars)}
-itos: Dict = lambda ints: { i: ch for i, ch in enumerate(ints)}
+d_train = load_dataset("tiny_shakespeare", split="train")[0]["text"]
+d_val = load_dataset("tiny_shakespeare", split="validation")[0]["text"]
+
+vocab = sorted(list(set(d_train + d_val)))
+
+stoi = {ch: i for i, ch in enumerate(vocab)}
+itos = {i: ch for i, ch in enumerate(vocab)}
+
+
+def encode(s: str):
+    return [stoi[c] for c in s]
+
+
+def decode(l: List):
+    return "".join([itos[i] for i in l])
